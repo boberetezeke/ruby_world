0b2ab8a77f1b4a69a6c7ac126a2cfcefcbb98b54
Steve Tuckner <stevetuckner@stewdle.com>
2009-10-11 16:55:11 -0500

    less output

--- install.rb
+++ install.rb
@@ -3,7 +3,7 @@ require "ftools"
 %w{javascripts stylesheets images}.each do |subdir|
 	Dir["vendor/plugins/ruby_world/public/#{subdir}/*"].each do |fn| 
 		dest_fn = "public/#{subdir}/#{File.basename(fn)}"
-		puts "copying #{fn} to #{dest_fn}"
+		puts "copying #{File.basename(fn)} to #{dest_fn}"
 		begin
 			File.copy(fn, dest_fn)
 		rescue Exception => e
