5473760396c5e3d8b7ce83ee0f3f5aca2c631342
Steve Tuckner <stevetuckner@stewdle.com>
2009-10-11 16:47:35 -0500

    more debug

--- install.rb
+++ install.rb
@@ -1,9 +1,10 @@
 require "ftools"
 
+puts "Dir.getwd = #{Dir.getwd}"
 %w{javascripts stylesheets images}.each do |subdir|
 	Dir["vendor/plugins/ruby_world/public/#{subdir}/*"].each do |fn| 
 		dest_fn = "../../../public/#{subdir}/#{File.basename(fn)}"
-		puts "copying #{File.basename(fn)} to #{dest_fn}"
+		puts "copying #{fn} to #{dest_fn}"
 		begin
 			File.copy(fn, dest_fn)
 		rescue Exception => e
