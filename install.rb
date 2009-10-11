c4bfa0efb4ee0dbcd7967bf34b7fdd996c179f4b
Steve Tuckner <stevetuckner@stewdle.com>
2009-10-11 16:41:34 -0500

    change directory that we start copies from

--- install.rb
+++ install.rb
@@ -1,7 +1,7 @@
 require "ftools"
 
 %w{javascripts stylesheets images}.each do |subdir|
-	Dir["public/#{subdir}/*"].each do |fn| 
+	Dir["vendor/plugins/ruby_world/public/#{subdir}/*"].each do |fn| 
 		dest_fn = "../../../public/#{subdir}/#{File.basename(fn)}"
 		puts "copying #{File.basename(fn)} to #{dest_fn}"
 		begin
