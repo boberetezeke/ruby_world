06fdbd3ab1e731fd00d721d34842481c1eeab9ef
Steve Tuckner <steve@steve-laptop.(none)>
2009-10-24 23:23:34 -0500

    added flash thingy for syntax highlighter

--- install.rb
+++ install.rb
@@ -11,7 +11,7 @@ end
 
 # copy public files
 def copy_public_files
-  %w{javascripts stylesheets images}.each do |subdir|
+  %w{javascripts stylesheets images flash}.each do |subdir|
     Dir["vendor/plugins/ruby_world/public/#{subdir}/*"].each do |fn| 
       dest_fn = "public/#{subdir}/#{File.basename(fn)}"
       puts "copying #{File.basename(fn)} to #{dest_fn}"
